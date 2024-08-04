0cce255228e1a5370b7a65b73ede411b7358301a
Kaan Sonmez <83972809+kaansnmez@users.noreply.github.com>
2024-08-04 16:43:43 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 import os
 def main():
+  desired_order_list=["type","project_id","private_key_id","private_key","client_email","client_id","auth_uri","token_uri",
+                        "auth_provider_x509_cert_url","client_x509_cert_url","universe_domain"]
+  service_dict={}
+  
   for key,value in os.environ.items():
-    print('{} : {}'.format(key,value))
+      if (key!='api_key') & (key!='api_secret'):
+          service_dict[key]=value
+  
+  SERVICE_ACCOUNT_FILE = {k: service_dict[k] for k in desired_order_list}
+  print(SERVICE_ACCOUNT_FILE)
 if __name__=='__main__':
     main()
